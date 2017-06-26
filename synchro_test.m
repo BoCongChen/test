9dbf4864257b1a6fe1937c44fb7d91b945eecf24
BoCongChen <onion1120@hotmail.com.tw>
2017-06-26 13:17:02 +0800

    Add files via upload

---
+++ synchro_test.m
@@ -0,0 +1,27 @@
+clear;clc
+x = 0 : 0.001 : 1 ;
+r = 3.4 ; e = 0 ; g = 0.3 ;
+L = 10 ; N = L^2 ;
+A = netgenerate([L L],'square') ;
+step = 100 ;
+
+x0 = x ;
+xx = rand(N-1,1) ;
+
+plot(x,x0,'b.')
+title('n = 0')
+axis([0 1 0 1])
+pause
+
+for n = 1 : step
+    for xi = 1 : length(x)
+        his = netdev(A,[r g e],'step',1,'initialstate',[x0(xi);xx]) ;
+        x0(xi) = his(1) ;
+    end
+    xx = his(2:end) ;
+    
+    plot(x,x0,'b.')
+    title(['n = ',num2str(n)])
+    axis([0 1 0 1])
+    pause
+end
